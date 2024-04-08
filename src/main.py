3eefcfe4404956f642d14ef955a4d94a22e10237
zunairaIkram <zunaira.ikram001@gmail.com>
2024-04-09 03:51:34 +0500

    Structured the project & Histogram Eq implementation in progress

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from data_loader import read_images 
+
+# Define the path to the root folder containing subfolders with images
+root_folder = r"data\train\images"
+
+# Call the function to start processing images
+read_images(root_folder)
+
+# for frame in frames:
+#     print(f"{frames[i]}\n")
